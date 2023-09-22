90cd5b17bdb9004b42c17685fd59a73cf6b0d788
Patrick McClelland <patrickmcclelland@Patricks-MBP.attlocal.net>
2023-09-21 19:44:08 -0700

    updated the server file. able to now retrieve all vehicle data

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
-const express = require("express");
-const session = require("express-session");
-const homeRoutes = require("./controllers/homeRoutes");
+// Import required packages and modules
+const express = require('express');
+const session = require('express-session');
+const { vehicleRoutes } = require('./controllers');
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-const sequelize = require("./config/connection");
-const SequelizeStore = require("connect-session-sequelize")(session.Store);
-
+// Create an Express application
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Session configuration
 const sess = {
-  secret: "Super secret secret",
+  secret: 'Super secret secret',
   cookie: {},
   resave: false,
   saveUninitialized: true,
@@ -18,14 +20,18 @@ const sess = {
   }),
 };
 
+// Use session middleware with the defined session configuration
 app.use(session(sess));
 
+// Parse JSON request bodies
 app.use(express.json());
+// Parse URL-encoded request bodies
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/', homeRoutes);
-app.use('/api', homeRoutes);
+// Mount vehicle routes under "/api"
+app.use('/api', vehicleRoutes);
 
+// Synchronize Sequelize models with the database and start the server
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log(`Now listening on Port ${PORT}`));
+  app.listen(PORT, () => console.log(`Now listening on http://localhost:${PORT}`));
 });
