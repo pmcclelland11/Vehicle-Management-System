d83896f165eec267ed01414876e52123d7fea250
Kowther Kahin <kowtherkahin@Kowthers-MacBook-Pro.local>
2023-09-22 17:46:19 -0700

    adds the handelbars files and renders all the vechicle data

--- controllers/index.js
+++ controllers/index.js
@@ -1,13 +1,11 @@
 // This file can serve as the main entry point for the controllers, 
 // where we import and initialize all the route files. 
 // It provides a clean and centralized way to set up your routes.
-
+const router = require("express").Router()
 const homeRoutes = require('./homeRoutes');
-const userRoutes = require('./api/userRoutes');
-const vehicleRoutes = require('./api/vehicleRoutes');
+const apiRoutes = require('./api');
 
-module.exports = {
-  homeRoutes,
-  userRoutes,
-  vehicleRoutes,
-};
+router.use("/", homeRoutes)
+router.use("/api", apiRoutes)
+
+module.exports = router;
