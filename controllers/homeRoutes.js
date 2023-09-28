c4d175b047e613c294c23a0d14bcfb8bea9e6dbd
Patrick McClelland <patrickmcclelland@Patricks-MBP.attlocal.net>
2023-09-28 03:13:42 -0700

    working on userRoutes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,9 +1,9 @@
 // This file can contain routes and controllers related to general application functionality, 
 // such as rendering the homepage or handling non-API requests.
-
 const express = require('express');
 const router = express.Router();
 const { Vehicle } = require("../models")
+const { User } = require("../models");
 
 // Route for the homepage
 router.get('/', (req, res) => {
@@ -17,11 +17,23 @@ router.get('/', (req, res) => {
 router.get('/login', (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
-    res.redirect('/vehicle');
+    res.redirect('/');
     return;
   }
 
   res.render('login');
+});
+
+// Route for logging out
+router.get('/logout', (req, res) => {
+  // Clear the user session data
+  req.session.destroy((err) => {
+    if (err) {
+      console.error('Error destroying session:', err);
+    }
+    // Redirect the user to the homepage
+    res.redirect('/');
+  });
 });
 
 // Route for the vehicle inventory
@@ -35,6 +47,38 @@ console.log(vehicles)
   } catch(err) {
     res.status(500).json(err)
     console.log(err)
+  }
+});
+
+// Route for handling user login (POST request)
+router.post('/api/users/login', async (req, res) => {
+  try {
+    const { your_name, your_pass } = req.body;
+
+    // Find the user by their username
+    const user = await User.findOne({ where: { username: your_name } });
+
+    if (!user) {
+      // User not found, render login page with an error message
+      res.render('login', { error: 'User not found' });
+      return;
+    }
+
+    // Check if the password is correct (you should use a secure password hashing library)
+    if (user.password !== your_pass) {
+      // Incorrect password, render login page with an error message
+      res.render('login', { error: 'Incorrect password' });
+      return;
+    }
+
+    // Set user session as logged in
+    req.session.logged_in = true;
+
+    // Redirect to the homepage after successful login
+    res.redirect('/');
+  } catch (error) {
+    console.error('Login error:', error);
+    res.status(500).json({ message: 'Login failed' });
   }
 });
 
