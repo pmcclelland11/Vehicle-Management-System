fb46cb49c43fe93cbb084743021e27139782e359
andrettaj <andrettaj@aol.com>
2023-09-27 19:41:01 -0700

    removed conflicts

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,13 +1,3 @@
-<<<<<<< HEAD
-const router = require('express').Router();
-const userRoutes = require('./userRoutes');
-const projectRoutes = require('./projectRoutes');
-
-router.use('/users', userRoutes);
-router.use('/projects', projectRoutes);
-
-module.exports = router;
-=======
 // This directory and file can be used to group all your API-related routes together.
 // It provides a clear separation between your API routes and other routes. 
 
@@ -22,4 +12,3 @@ router.use('/users', userRoutes);
 router.use('/vehicles', vehicleRoutes);
 
 module.exports = router;
->>>>>>> d80c149c34c5aabf1fb63b07e8de2dabbeaa559b
--- controllers/index.js
+++ controllers/index.js
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-const router = require('express').Router();
-
-const apiRoutes = require('./api');
-const homeRoutes = require('./homeRoutes');
-
-router.use('/', homeRoutes);
-router.use('/api', apiRoutes);
-
-module.exports = router;
-=======
 // This file can serve as the main entry point for the controllers, 
 // where we import and initialize all the route files. 
 // It provides a clean and centralized way to set up your routes.
@@ -20,4 +9,3 @@ router.use("/", homeRoutes);
 router.use("/api", apiRoutes);
 
 module.exports = router;
->>>>>>> d80c149c34c5aabf1fb63b07e8de2dabbeaa559b
