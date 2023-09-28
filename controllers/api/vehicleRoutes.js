c22d4f1883cd80e9bab003357c3646fd90e3b3a7
Patrick McClelland <patrickmcclelland@Patricks-MBP.attlocal.net>
2023-09-27 21:19:53 -0700

    add new vehicle feature is now working properly

--- controllers/api/vehicleRoutes.js
+++ controllers/api/vehicleRoutes.js
@@ -7,16 +7,13 @@ const { requireAuth, requireManagerAuth } = require("../../middleware/authMiddle
 const { body, validationResult } = require("express-validator"); // New Library: express-validator
 
 // ALL GET ROUTES: Use to filter vehicles in the database
-// Get all vehicles
-router.get("/vehicles", requireAuth, async (req, res) => {
+// GET Route for /api/vehicles
+router.get("/", async (req, res) => {
   try {
-    // Retrieve all vehicles from the database
+    // Your logic to retrieve vehicles goes here
     const vehicles = await Vehicle.findAll();
-
-    // Respond with the list of vehicles
     res.json(vehicles);
   } catch (error) {
-    // Handle database error
     console.error(error);
     res.status(500).json({ error: "Failed to retrieve vehicles." });
   }
@@ -44,57 +41,59 @@ router.get("/vehicles/:type", async (req, res) => {
 // POST Route
 // Create a new vehicle (Note: only accessible to managers)
 router.post(
-  "/vehicles",
+  "/",
   // requireManagerAuth,
   [
-    // Define validation rules for each field in the request body
-    body("make").trim().notEmpty().withMessage("Make is required"),
-    body("model").trim().notEmpty().withMessage("Model is required"),
-    body("year")
-      .isInt({ min: 1900, max: new Date().getFullYear() })
-      // .isInt method is from the express-validator library
-      // "new Date().getFullYear()" returns the current year
-      .withMessage("Year must be a valid year"),
-    body("mileage")
-      .isInt({ min: 0 })
-      .withMessage("Mileage must be a positive integer"),
-    body("type").trim().notEmpty().withMessage("Type is required"),
-    body("price")
-      .isDecimal({ decimal_digits: "2" })
-      .withMessage("Price must be a valid decimal"),
-    body("isSold").isBoolean().withMessage("isSold must be a boolean"),
+      // Define validation rules for each field in the request body
+      body("make").trim().notEmpty().withMessage("Make is required"),
+      body("model").trim().notEmpty().withMessage("Model is required"),
+      body("year")
+          .isInt({ min: 1900, max: new Date().getFullYear() })
+          // .isInt method is from the express-validator library
+          // "new Date().getFullYear()" returns the current year
+          .withMessage("Year must be a valid year"),
+      body("mileage")
+          .isInt({ min: 0 })
+          .withMessage("Mileage must be a positive integer"),
+      body("type").trim().notEmpty().withMessage("Type is required"),
+      body("price")
+          .isDecimal({ decimal_digits: "2" })
+          .withMessage("Price must be a valid decimal"),
+      body("isSold").isBoolean().withMessage("isSold must be a boolean"),
   ],
   async (req, res) => {
-    try {
-      // Check for validation errors
-      const errors = validationResult(req);
+      try {
+          // Check for validation errors
+          const errors = validationResult(req);
 
-      if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
+          if (!errors.isEmpty()) {
+              return res.status(400).json({ errors: errors.array() });
+          }
+
+          // Extract validated vehicle data from the request body
+          const { make, model, year, mileage, type, price, isSold } = req.body;
+          console.log(req.body);
+
+          // Create a new vehicle in the database
+          const newVehicle = await Vehicle.create({
+              make,
+              model,
+              year,
+              mileage,
+              type,
+              price,
+              isSold,
+          });
+
+          // Respond with the newly created vehicle
+          res.status(201).json(newVehicle);
+      } catch (error) {
+          // Handle vehicle creation error
+          console.error(error);
+          res.status(500).json({ error: "Vehicle creation failed." });
       }
-
-      // Extract validated vehicle data from the request body
-      const { make, model, year, mileage, type, price, isSold } = req.body;
-
-      // Create a new vehicle in the database
-      const newVehicle = await Vehicle.create({
-        make,
-        model,
-        year,
-        mileage,
-        type,
-        price,
-        isSold,
-      });
-
-      // Respond with the newly created vehicle
-      res.json(newVehicle);
-    } catch (error) {
-      // Handle vehicle creation error
-      console.error(error);
-      res.status(500).json({ error: "Vehicle creation failed." });
-    }
   }
+  
 );
 
 // PUT Route:
