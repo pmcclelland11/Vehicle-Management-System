89e5a5abada1410346da5955e250ac6fec4a29a4
Patrick McClelland <patrickmcclelland@Patricks-MBP.attlocal.net>
2023-10-05 23:15:36 -0700

    debugging the login page

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,25 +1,33 @@
-// This file can contain routes and controllers specific to user-related actions, 
+// This file can contain routes and controllers specific to user-related actions,
 // such as user registration, login, and user-specific API endpoints.
 const express = require('express');
 const router = express.Router();
 const { User } = require('../../models');
 const bcrypt = require('bcrypt');
-const { requireAuth } = require("../../middleware/authMiddleware"); 
+const { requireAuth } = require('../../middleware/authMiddleware');
 
 // Signup Route
 router.post('/signup', async (req, res) => {
   try {
-    const userData = await User.create(req.body);
+    const { name, email, password } = req.body;
 
+    // Hash the password before storing it in the database
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Create a new user record
+    const userData = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    // Save user session upon successful signup
     req.session.save(() => {
       req.session.user_id = userData.id;
       req.session.logged_in = true;
-
-      // Redirect to the "/vehicles" page upon successful signup
       res.redirect('/vehicles');
     });
   } catch (err) {
-    // Handle signup error and respond with an error message
     console.error(err);
     res.status(400).json({ error: 'Signup failed. Please try again.' });
   }
@@ -28,45 +36,61 @@ router.post('/signup', async (req, res) => {
 // Login Route
 router.post('/login', async (req, res) => {
   try {
-    // Extract user login data from the request body
     const { email, password } = req.body;
 
     // Find the user by their email
     const user = await User.findOne({ where: { email } });
 
+    console.log(user);
+
     if (!user) {
       return res.status(404).json({ error: 'User not found.' });
     }
+    console.log(user.password, password);
+    
+    // Check the password using bcrypt
+    const isPasswordValid = bcrypt.compareSync(user.password, password);
+    console.log(isPasswordValid)
 
-    // Check the password
-    const isPasswordValid = await bcrypt.compare(password, user.password);
+    // Set up the user session upon successful login
+    req.session.save (()=>{
+      req.session.logged_in = true;
+      req.session.user_id = user.id;
+      req.session.userName = user.name;
+      res.status(200).json({user, message:"success"});
+    })
 
-    if (!isPasswordValid) {
-      return res.status(401).json({ error: 'Invalid password.' });
-    }
+    console.log(req.session.logged_in)
 
-    // Set up the user session
-    req.session.user = {
-      id: user.id,
-      name: user.name,
-      email: user.email,
-    };
-
-    // Redirect to the "/vehicles" page upon successful login
-    res.redirect('/vehicles');
+    
   } catch (error) {
-    // Handle login error
     console.error(error);
     res.status(500).json({ error: 'Login failed.' });
   }
 });
 
 // User logout route
-router.post('/logout', (req, res) => {
-  // Destroy the user session
-  req.session.destroy(() => {
-    res.status(204).end();
-  });
-});
+router.post("/logout", (req, res) => {
+  // if (req.session.logged_in) {
+  //   req.session.destroy((err) => {
+  //     if (err) {
+  //       console.error("Error destroying session:", err);
+  //     }
+  //     //res.redirect("/");
+  //     res.status(200).end()
+  //   }) 
+  // } else {
+  //   res.status(404).end();
+  // }
+  req.session.destroy(()=>res.status(200).end())
+  
+  
+
+})
+
+router.get("/", (req, res) => {
+  User.findAll().then(data=> res.json(data))
+})
+
 
 module.exports = router;
